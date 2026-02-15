63cd357dc9c219c0d0faa939b8e7a994861b44c0
melihaydogan377 <melihaydogan377@gmail.com>
2026-02-16 01:16:12 +0300

    Delete haberler.js

--- haberler.js
+++
@@ -1,8 +0,0 @@
-const haberHavuzu = {
-    "linkedin.egitimi": {
-        baslik: "Etkili LinkedIn Yönetimi Semineri",
-        tarih: "03 Şubat 2026", // Yeni alan
-        resim: "etkinlik1.jpg",
-        metin: `Metin buraya...`
-    }
-};
